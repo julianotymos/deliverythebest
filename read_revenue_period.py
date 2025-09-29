e78478ae4c7094acebb920ffa4ff641964821495
julianotymos <149689888+julianotymos@users.noreply.github.com>
2025-09-29 08:24:22 -0300

    Ajustado fuso para pedidos

--- read_revenue_period.py
+++ read_revenue_period.py
@@ -44,7 +44,7 @@ def read_revenue_period(start_date: date, end_date: date, sales_channel: str = N
 
     query = f"""
     SELECT
-        DATE(ot.CREATED_AT) AS order_date,
+        DATE(ot.CREATED_AT, "America/Sao_Paulo") AS order_date,
         STRING_AGG(DISTINCT OT.SALES_CHANNEL, ', ' ORDER BY OT.SALES_CHANNEL) AS Canais,
         SUM(bi.sub_total_value) AS revenue,
         SUM(p.cost * bi.quantity) AS cost,
@@ -86,11 +86,11 @@ def read_revenue_period(start_date: date, end_date: date, sales_channel: str = N
 INNER JOIN SALES_CHANNEL CH ON CH.ID = P.SALES_CHANNEL) p
         ON p.name = bi.name
         AND p.sales_channel = OT.SALES_CHANNEL
-        AND DATE(OT.CREATED_AT) BETWEEN P.VALID_FROM_DATE AND P.VALID_TO_DATE
+        AND DATE(ot.CREATED_AT, "America/Sao_Paulo") BETWEEN P.VALID_FROM_DATE AND P.VALID_TO_DATE
     LEFT JOIN CUSTOMER C ON C.ID = OT.CUSTOMER_ID
 
     LEFT JOIN (
-        SELECT DATE(ot.CREATED_AT) AS order_date,
+        SELECT DATE(ot.CREATED_AT, "America/Sao_Paulo") AS order_date,
                COUNT(1) AS QTY_PEDIDOS,
                SUM(
                    CASE 
@@ -107,24 +107,24 @@ INNER JOIN SALES_CHANNEL CH ON CH.ID = P.SALES_CHANNEL) p
                    END
                ) AS CLIENTES_RECORRENTES
         FROM ORDERS_TABLE ot
-        WHERE DATE(ot.CREATED_AT) BETWEEN '{start_date_str}' AND '{end_date_str}'
+        WHERE DATE(ot.CREATED_AT, "America/Sao_Paulo") BETWEEN '{start_date_str}' AND '{end_date_str}'
         {where_channel_clause} -- Adicionei o filtro aqui
         {where_customer_clause}  -- <-- agora entra junto com AND
 
-        GROUP BY DATE(ot.CREATED_AT)
-    ) QOT ON QOT.order_date = DATE(ot.CREATED_AT)
+        GROUP BY DATE(ot.CREATED_AT, "America/Sao_Paulo")
+    ) QOT ON QOT.order_date = DATE(ot.CREATED_AT, "America/Sao_Paulo")
 
     WHERE ot.current_status IN ('CONCLUDED', 'PARTIALLY_CANCELLED')
-      AND DATE(ot.CREATED_AT) BETWEEN '{start_date_str}' AND '{end_date_str}'
+      AND DATE(ot.CREATED_AT, "America/Sao_Paulo") BETWEEN '{start_date_str}' AND '{end_date_str}'
       {where_channel_clause} -- E adicionei o filtro aqui também
       {where_customer_clause}  -- <-- agora entra junto com AND
 
     GROUP BY
-        DATE(ot.CREATED_AT),
+        DATE(ot.CREATED_AT, "America/Sao_Paulo"),
         QOT.QTY_PEDIDOS,
         QOT.NOVOS_CLIENTES,
         QOT.CLIENTES_RECORRENTES
-    ORDER BY DATE(ot.CREATED_AT) DESC
+    ORDER BY DATE(ot.CREATED_AT, "America/Sao_Paulo") DESC
     """
 
     try:
